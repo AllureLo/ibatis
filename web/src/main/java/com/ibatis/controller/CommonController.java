fde5990342c5fa6cb33fac059fb14a1d2c93a458
Chenjinhang <chenjinhang@sohh.cn>
2019-08-22 18:40:00 +0800

    x

--- web/src/main/java/com/ibatis/controller/CommonController.java
+++ web/src/main/java/com/ibatis/controller/CommonController.java
@@ -1,8 +1,11 @@
 package com.ibatis.controller;
 
+import com.ibatis.base.BaseModel;
 import com.ibatis.bean.Result;
 import com.ibatis.model.po.Test;
+import com.ibatis.model.po.TestTwo;
 import com.ibatis.service.TestService;
+import com.ibatis.service.TestTwoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,18 +26,34 @@ public class CommonController {
     @Autowired
     private TestService testService;
 
+    @Autowired
+    private TestTwoService testTwoService;
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public Result add(@RequestParam(value = "param") String param) {
-        Test test = new Test();
-        test.setParam(param);
-        testService.save(test);
-        return Result.success(test);
+    public Result add(@RequestParam(value = "param") String param ,
+                      @RequestParam(value = "type", required = false, defaultValue = "0") Integer type) {
+        if (type == 0) {
+            Test test = new Test();
+            test.setParam(param);
+            testService.save(test);
+        } else {
+            TestTwo testTwo = new TestTwo();
+            testTwo.setParam(param);
+            testTwoService.save(testTwo);
+        }
+        return Result.success();
     }
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public Result get(@RequestParam(value = "id") Long id) {
-        Test test = testService.queryById(id);
-        return Result.success(test);
+    public Result get(@RequestParam(value = "id") Long id,
+                      @RequestParam(value = "type", required = false, defaultValue = "0") Integer type) {
+        BaseModel baseModel;
+        if (type == 0) {
+            baseModel = testService.queryById(id);
+        } else {
+            baseModel = testTwoService.queryById(id);
+        }
+        return Result.success(baseModel);
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
